d0dac580066e32eac4e25d694b7b22f00e56597d
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-10-10 21:46:18 +0200

    add user admin

--- routes/users.py
+++ routes/users.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from database import web_users, database, Session
 from models import UpdateUserRole, Users
 from fastapi.templating import Jinja2Templates
 from dependencies import get_token_from_cookie, get_current_user
+from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -26,31 +28,9 @@ async def get_users(request: Request):
     query = web_users.select()
     users_data = await database.fetch_all(query)
     session = Session()
-    users_tg = session.query(Users).all()
 
     # Формируем список словарей с данными пользователей
-    users_tg_data = []
-    for user in users_tg:
-        users_tg_data.append(
-            {
-                "id": user.id,
-                "tg": user.tg,
-                "full_name": user.full_name,
-                "is_manager": user.is_manager,
-                "is_instructor": user.is_instructor,
-                "is_assistant": user.is_assistant,
-                "send_button": user.send_button,
-                "deposit_income": user.deposit_income,
-                "enter_operation": user.enter_operation,
-                "view_salary": user.view_salary,
-                "contribute_expense": user.contribute_expense,
-                "is_director": user.is_director,
-                "chat_id": user.chat_id,
-                "comission": user.comission,
-                "penalty": user.penalty,
-                "is_investor": user.is_investor,
-            }
-        )
+    users_tg_data = session.query(Users).all()
     await database.disconnect()
 
     # Возвращаем HTML-шаблон с данными пользователей
@@ -73,6 +53,80 @@ async def update_user_role(user_id: int, role_update: UpdateUserRole):
     await database.disconnect()
     return {"message": "User role updated"}
 
+
+@router.post("/update_tg_users")
+async def update_users(
+    user_id: int = Form(...),
+    tg: str = Form(...),
+    full_name: str = Form(...),
+    is_manager: bool = Form(False),
+    is_instructor: bool = Form(False),
+    is_assistant: bool = Form(False),
+    send_button: bool = Form(False),
+    deposit_income: bool = Form(False),
+    enter_operation: bool = Form(False),
+    view_salary: bool = Form(False),
+    contribute_expense: bool = Form(False),
+    is_director: bool = Form(False),
+    chat_id: int = Form(...),
+    comission: bool = Form(False),
+    penalty: bool = Form(False),
+    is_investor: bool = Form(False),
+):
+    session = Session()
+    try:
+        user = session.query(Users).filter(Users.id == user_id).first()
+        if not user:
+            return {"error": "User not found"}
+
+        # Обновление полей пользователя
+        user.tg = tg
+        user.full_name = full_name
+        user.is_manager = is_manager
+        user.is_instructor = is_instructor
+        user.is_assistant = is_assistant
+        user.send_button = send_button
+        user.deposit_income = deposit_income
+        user.enter_operation = enter_operation
+        user.view_salary = view_salary
+        user.contribute_expense = contribute_expense
+        user.is_director = is_director
+        user.chat_id = chat_id
+        user.comission = comission
+        user.penalty = penalty
+        user.is_investor = is_investor
+
+        session.commit()
+    finally:
+        session.close()
+
+    return RedirectResponse("/users", status_code=303)
+
+@router.post("/update_user_tg/")
+async def update_user(data: dict):
+    session = Session()
+    user = session.query(Users).filter(Users.id == data["id"]).first()
+    if user:
+        user.tg = data['tg']
+        user.full_name = data['full_name']
+        user.is_manager = data['is_manager']
+        user.is_instructor = data['is_instructor']
+        user.is_assistant = data['is_assistant']
+        user.send_button = data['send_button']
+        user.deposit_income = data['deposit_income']
+        user.enter_operation = data['enter_operation']
+        user.view_salary = data['view_salary']
+        user.contribute_expense = data['contribute_expense']
+        user.is_director = data['is_director']
+        user.chat_id = data['chat_id']
+        user.comission = data['comission']
+        user.penalty = data['penalty']
+        user.is_investor = data['is_investor']
+        session.commit()
+        session.close()
+        return JSONResponse({"status": "success"})
+    session.close()
+    return JSONResponse({"status": "error"})
 
 # Удаление пользователя
 @router.delete("/users/{user_id}")
