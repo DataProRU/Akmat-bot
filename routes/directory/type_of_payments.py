7cdd141cb7a2140e2087b9b313c0fe9a9205b92a
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-10-22 21:29:02 +0200

    payments and technicue made

--- routes/directory/type_of_payments.py
+++ routes/directory/type_of_payments.py
@@ -1,14 +1,90 @@
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi import  Request
+from dependencies import get_token_from_cookie, get_current_user
+from fastapi.responses import RedirectResponse
+from fastapi import  Request, Depends
+from database import Session
+from models import PaymentTypes
+from fastapi import Form
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
 @router.get("/type_of_payments", response_class=HTMLResponse)
-async def directory(request: Request):
+async def directory(request: Request, db: Session = Depends(get_db)):
+    # Получаем токен
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token  # Если токен отсутствует, перенаправляем на страницу логина
+
+    # Получаем информацию о текущем пользователе
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload  # Если токен недействителен, перенаправляем на страницу логина
+
+    type_payments = db.query(PaymentTypes).all()
 
     return templates.TemplateResponse("directory/type_of_payments.html", {
         "request": request,
+        "type_payments": type_payments,
     })
+
+@router.post("/type_of_payments/add", response_class=HTMLResponse)
+async def add_type_payments(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    new_type = PaymentTypes(title=title)
+    db.add(new_type)
+    db.commit()
+
+    return RedirectResponse(url="/type_of_payments", status_code=303)
+
+@router.post("/type_of_payments/edit/{id}", response_class=HTMLResponse)
+async def edit_type_payments(request: Request, id: int, title: str = Form(...), db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    # Поиск записи по id
+    type_to_edit = db.query(PaymentTypes).filter(PaymentTypes.id == id).first()
+    if type_to_edit:
+        type_to_edit.title = title  # Обновляем название
+        db.commit()
+
+    return RedirectResponse(url="/type_of_payments", status_code=303)
+
+@router.post("/type_of_payments/delete/{id}", response_class=HTMLResponse)
+async def delete_type_payments(request: Request, id: int, db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    type_to_delete = db.query(PaymentTypes).filter(PaymentTypes.id == id).first()
+    if type_to_delete:
+        db.delete(type_to_delete)
+        db.commit()
+
+    return RedirectResponse(url="/type_of_payments", status_code=303)
+
--- routes/directory/type_of_technique.py
+++ routes/directory/type_of_technique.py
@@ -5,6 +5,7 @@ from dependencies import get_token_from_cookie, get_current_user
 from fastapi.responses import RedirectResponse
 from fastapi import APIRouter, Request, Depends
 from models import TypeTechniques
+from fastapi import Form
 
 router = APIRouter()
 
@@ -36,3 +37,55 @@ async def directory(request: Request, db: Session = Depends(get_db)):
         "request": request,
         "type_techniques": type_techniques
     })
+
+@router.post("/type_of_technique/add", response_class=HTMLResponse)
+async def add_type_technique(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    new_type = TypeTechniques(title=title)
+    db.add(new_type)
+    db.commit()
+
+    return RedirectResponse(url="/type_of_technique", status_code=303)
+
+@router.post("/type_of_technique/edit/{id}", response_class=HTMLResponse)
+async def edit_type_technique(request: Request, id: int, title: str = Form(...), db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    # Поиск записи по id
+    type_to_edit = db.query(TypeTechniques).filter(TypeTechniques.id == id).first()
+    if type_to_edit:
+        type_to_edit.title = title  # Обновляем название
+        db.commit()
+
+    return RedirectResponse(url="/type_of_technique", status_code=303)
+
+@router.post("/type_of_technique/delete/{id}", response_class=HTMLResponse)
+async def delete_type_technique(request: Request, id: int, db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    type_to_delete = db.query(TypeTechniques).filter(TypeTechniques.id == id).first()
+    if type_to_delete:
+        db.delete(type_to_delete)
+        db.commit()
+
+    return RedirectResponse(url="/type_of_technique", status_code=303)
+
