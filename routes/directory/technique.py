df555c425458b1386fed3229a4767f059003947e
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-10-23 20:22:23 +0200

    add technique

--- routes/directory/technique.py
+++ routes/directory/technique.py
@@ -38,3 +38,74 @@ async def directory(request: Request, db: Session = Depends(get_db)):
         "type_techniques": type_techniques,
         "investors": investors
     })
+
+@router.post("/technique/edit/{id}", response_class=HTMLResponse)
+async def edit_technique(
+    request: Request,
+    id: int,
+    title: str = Form(...),
+    type_technique_id: int = Form(...),
+    investor_id: int = Form(...),
+    visible: bool = Form(False),
+    db: Session = Depends(get_db),
+):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    # Поиск записи по id
+    type_to_edit = db.query(Techniques).filter(Techniques.id == id).first()
+    if type_to_edit:
+        type_to_edit.title = title
+        type_to_edit.type_technique_id = type_technique_id
+        type_to_edit.investor_id = investor_id
+        type_to_edit.visible = visible
+        db.commit()
+
+    return RedirectResponse(url="/technique", status_code=303)
+
+@router.post("/technique/add", response_class=HTMLResponse)
+async def add_technique(
+    request: Request,
+    title: str = Form(...),
+    type_technique_id: int = Form(...),
+    investor_id: int = Form(...),
+    visible: bool = Form(False),
+    db: Session = Depends(get_db),
+):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    new_tech = Techniques(title=title,
+                           type_technique_id=type_technique_id,
+                           investor_id=investor_id,
+                          visible = visible)
+    db.add(new_tech)
+    db.commit()
+
+    return RedirectResponse(url="/technique", status_code=303)
+@router.post("/technique/delete/{id}", response_class=HTMLResponse)
+async def delete_technique(request: Request, id: int, db: Session = Depends(get_db)):
+    token = get_token_from_cookie(request)
+    if isinstance(token, RedirectResponse):
+        return token
+
+    payload = get_current_user(token)
+    if isinstance(payload, RedirectResponse):
+        return payload
+
+    tech_to_delete = db.query(Techniques).filter(Techniques.id == id).first()
+    if tech_to_delete:
+        db.delete(tech_to_delete)
+        db.commit()
+
+    return RedirectResponse(url="/technique", status_code=303)
