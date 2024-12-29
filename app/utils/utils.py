ffcdbbcfab670a33697fbfb33c2d3b0fc09f1080
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-12-29 20:30:53 +0200

    Update utils.py

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -8,15 +8,14 @@ from aiogram.types import Message
 
 # Собственные модули
 from app.utils.token_utils import prepare_user_info, generate_token
-from app.config import ADD_CHAT_ID_URL
+from app.config import ADD_CHAT_ID_URL, BASE_URL
+from app.tinkoff.utils import send_buttons
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 
 
 async def check_user_access(message: Message):
     """
     Отправляет запрос к API для проверки доступа.
-    :param tg_nickname: Telegram никнейм.
-    :param chat_id: Telegram chat_id.
-    :return: Ответ от API.
     """
     try:
         # Отправка запроса к API для проверки наличия пользователя в бд
@@ -28,12 +27,13 @@ async def check_user_access(message: Message):
                     raise Exception(await response.json())
                 return await response.json()
     except Exception as e:
-        # Если пришла ошибка то обрабатываем отправляем сообщение об ошибке пользователю
+        # Если возникла ошибка, отправляем сообщение об ошибке пользователю
         error_detail = getattr(e, 'args', ["Ошибка без деталей"])[0]
         if isinstance(error_detail, dict) and "detail" in error_detail:
             error_message = error_detail["detail"]
         else:
             error_message = str(error_detail)
+
         await message.answer(f"Ошибка: {error_message}")
         return
 
@@ -69,5 +69,3 @@ async def air_balon_and_swings(message: Message):
         text="Внесите доход",
         reply_markup=keyboard
     )
-
-    
