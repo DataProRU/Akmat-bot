d1ed4fb276c6ffb01fad9e927cd984721cba7eb7
Pavel Ershov <ershov@Pavels-MacBook-Air.local>
2025-01-12 20:08:41 +0200

    start form

--- routes/bot_air_balon.py
+++ routes/bot_air_balon.py
@@ -60,9 +60,7 @@ async def submit_balloon(
         return JSONResponse(content={"message": "Ошибка работы с таблицей"}, status_code=500)
 
     try:
-
         # Установим формат столбцов
-        worksheet.format('A:A', {"numberFormat": {"type": "TEXT"}})
         worksheet.format('C:C', {"numberFormat": {"type": "NUMBER"}})
 
         current_time = datetime.now(moscow_tz)
@@ -103,10 +101,9 @@ async def submit_balloon(
             if last_date.month != current_time.month or last_date.year != current_time.year:
                 # Добавить месяц и год в новую строку
                 worksheet.append_row([new_month_year], value_input_option="USER_ENTERED")
-            elif last_date < current_time:
+            elif last_date.date() < current_time.date():
                 # Добавить пустую строку с текущей датой, если дата последней записи меньше текущей
                 worksheet.append_row([new_date], value_input_option="USER_ENTERED")
-
 
         # Подготовить данные для новой строки
         new_row = [
@@ -117,112 +114,16 @@ async def submit_balloon(
             nalchik_free_flight,
             terminal_free_flight,
             weather,
+            "",
             comment,
         ]
 
         # Добавить новую строку с данными отчета
         worksheet.append_row(new_row, value_input_option="USER_ENTERED")
 
-
         return JSONResponse(content={"message": "Отчет успешно отправлен!"})
 
     except Exception as e:
-
         return JSONResponse(content={"message": "Ошибка сервера"}, status_code=500)
 
     return JSONResponse(content={"message": "Данные успешно отправлены"})
-
-
-from routes.directory.tinkoff_expenses import get_expenses_from_db
-from utils.tinkoff.time_utils import get_period_range
-
-def sync_expenses_to_sheet(db, period="week", timezone_str="Europe/Moscow"):
-    """
-    Синхронизация расходов из базы данных в Google Sheets. ВЕРСИЯ С АЙДИШНИКАМИ
-    """
-    # Получаем расходы за период
-    unix_range_start, unix_range_end = get_period_range(
-        timezone=timezone_str,
-        period=period
-    )
-
-    expenses_data = get_expenses_from_db(
-        db=db,
-        unix_range_start=unix_range_start,
-        unix_range_end=unix_range_end,
-        timezone_str=timezone_str
-    )
-
-    # Получаем текущие данные из таблицы
-    existing_rows = worksheet.get_all_values()
-    existing_expenses = {
-        row[0]: row[1:]  # Ключ — `id`, значение — остальные данные
-        for row in existing_rows[1:]  # Пропускаем заголовок
-    }
-
-    # Обрабатываем новые данные
-    for expense in expenses_data["expenses"]:
-        expense_id = str(expense["id"])
-        date_time = expense["date_time"]
-        card_number = expense["card_number"]
-        amount = "{:.2f}".format(expense["amount"]).rstrip('0').rstrip('.').replace('.', ',')
-        description = expense["description"]
-        category = expense["category"]
-
-        # Если запись с таким `id` есть
-        if expense_id in existing_expenses:
-            existing_data = existing_expenses[expense_id]
-
-            # Формируем строку для сравнения
-            updated_data = [date_time, card_number, amount, description, category]
-
-            # Если данные отличаются, обновляем запись
-            if existing_data != updated_data:
-                # Находим индекс строки для обновления
-                row_index = list(existing_expenses.keys()).index(expense_id) + 2  # +2 из-за заголовка
-                worksheet.update(f"B{row_index}:F{row_index}", [updated_data])
-        else:
-            # Если записи нет, добавляем новую строку
-            worksheet.append_row([expense_id, date_time, card_number, amount, description, category])
-
-
-@router.get("/submit_tinkoff")
-async def sync_expenses_to_sheet_no_id( period="month", timezone_str="Europe/Moscow",  db: Session = Depends(get_db)):
-    """
-    Синхронизация расходов из базы данных в Google Sheets. ВЕРСИЯ БЕЗ АЙДИШНИКОВ
-    """
-    # Получаем расходы за последние 7 дней
-    unix_range_start, unix_range_end = get_period_range(
-        timezone="Europe/Moscow",
-        period=period
-    )
-
-    expenses_data = get_expenses_from_db(
-        db=db,
-        unix_range_start=unix_range_start,
-        unix_range_end=unix_range_end,
-        timezone_str=timezone_str
-    )
-
-    # Получаем текущие данные из таблицы
-    existing_rows = worksheet.get_all_values()
-    existing_expenses = {
-        (row[0], row[1], row[2], row[3]): row[4]
-        for row in existing_rows[1:]  # Пропускаем заголовок
-    }
-    existing_expenses_tuples = [tuple(expense) for expense in existing_expenses]
-    print(expenses_data)
-
-    # Добавляем новые расходы
-    for expense in expenses_data["expenses"]:
-        date_time = expense["date_time"]
-        card_number = expense["card_number"]
-        amount = "{:.2f}".format(expense["amount"]).rstrip('0').rstrip('.').replace('.',',')
-        description = expense["description"]
-        category = expense["category"]
-        print(date_time, card_number, amount, description, category)
-
-        # Проверяем, есть ли запись в таблице
-        if (date_time, card_number, str(amount), description) not in existing_expenses_tuples:
-            print(date_time, card_number, amount, description, category)
-            worksheet.append_row([date_time, card_number, amount, description, category])
--- routes/bot_swing.py
+++ routes/bot_swing.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import logging
 from typing import List, Union
 from babel.dates import format_date
+from typing import Optional
 
 
 # Настройка логгера
@@ -58,7 +59,6 @@ async def directory(request: Request, username: str, db: Session = Depends(get_d
         logger.error(f"Ошибка при загрузке данных: {str(e)}")
         return JSONResponse(content={"message": "Ошибка сервера"}, status_code=500)
 
-
 @router.post("/send_report")
 async def send_report(
         username: str = Form(...),
@@ -77,33 +77,32 @@ async def send_report(
         if isinstance(shift, list):
             shift = ', '.join(shift)
 
-        # Установим формат столбцов
-        worksheet.format('A:A', {"numberFormat": {"type": "TEXT"}})
-        worksheet.format('C:C', {"numberFormat": {"type": "NUMBER"}})
+            # Установим формат столбцов
+            worksheet.format('C:C', {"numberFormat": {"type": "NUMBER"}})
 
-        current_time = datetime.now(moscow_tz)
-        new_date = format_date(current_time, "d MMMM", locale="ru")  # Формат: "5 января"
+            current_time = datetime.now(moscow_tz)
+            new_date = format_date(current_time, "d MMMM", locale="ru")  # Формат: "5 января"
 
-        # Массив месяцев в именительном падеже
-        months_nom = [
-            "ЯНВАРЬ", "ФЕВРАЛЬ", "МАРТ", "АПРЕЛЬ", "МАЙ", "ИЮНЬ",
-            "ИЮЛЬ", "АВГУСТ", "СЕНТЯБРЬ", "ОКТЯБРЬ", "НОЯБРЬ", "ДЕКАБРЬ"
-        ]
+            # Массив месяцев в именительном падеже
+            months_nom = [
+                "ЯНВАРЬ", "ФЕВРАЛЬ", "МАРТ", "АПРЕЛЬ", "МАЙ", "ИЮНЬ",
+                "ИЮЛЬ", "АВГУСТ", "СЕНТЯБРЬ", "ОКТЯБРЬ", "НОЯБРЬ", "ДЕКАБРЬ"
+            ]
 
-        # Формируем строку для месяца и года в именительном падеже
-        new_month_year = f"{months_nom[current_time.month - 1]} {current_time.year}"
+            # Формируем строку для месяца и года в именительном падеже
+            new_month_year = f"{months_nom[current_time.month - 1]} {current_time.year}"
 
-        # Словарь месяцев на русском языке (для старого кода с родительным падежом)
-        months_dict = {
-            "января": 1, "февраля": 2, "марта": 3, "апреля": 4, "мая": 5, "июня": 6,
-            "июля": 7, "августа": 8, "сентября": 9, "октября": 10, "ноября": 11, "декабря": 12
-        }
+            # Словарь месяцев на русском языке (для старого кода с родительным падежом)
+            months_dict = {
+                "января": 1, "февраля": 2, "марта": 3, "апреля": 4, "мая": 5, "июня": 6,
+                "июля": 7, "августа": 8, "сентября": 9, "октября": 10, "ноября": 11, "декабря": 12
+            }
 
-        # Получить все данные из столбца с датами
-        dates_column = worksheet.col_values(1)  # Считать столбец A (с датами)
-        if dates_column:
-            last_date_str = dates_column[-1]  # Последняя дата
-            try:
+            # Получить все данные из столбца с датами
+            dates_column = worksheet.col_values(1)  # Считать столбец A (с датами)
+            if dates_column:
+                last_date_str = dates_column[-1]  # Последняя дата
+
                 # Разделяем строку на день и месяц
                 day, month = last_date_str.split()  # "5 января" -> day = "5", month = "января"
                 month_number = months_dict[month]  # Получаем номер месяца из словаря
@@ -119,32 +118,29 @@ async def send_report(
                 if last_date.month != current_time.month or last_date.year != current_time.year:
                     # Добавить месяц и год в новую строку
                     worksheet.append_row([new_month_year], value_input_option="USER_ENTERED")
-                elif last_date < current_time:
+                elif last_date.date() < current_time.date():
                     # Добавить пустую строку с текущей датой, если дата последней записи меньше текущей
                     worksheet.append_row([new_date], value_input_option="USER_ENTERED")
 
-            except ValueError:
-                logger.warning(f"Некорректная дата в таблице: {last_date_str}")
+            # Подготовить данные для новой строки
+            new_row = [
+                new_date,
+                username,
+                klichka,
+                terminal,
+                checksCount,
+                shift,
+                additional,
+                reason,
+                comment,
+            ]
 
-        # Подготовить данные для новой строки
-        new_row = [
-            new_date,
-            username,
-            klichka,
-            terminal,
-            checksCount,
-            shift,
-            additional,
-            reason,
-            comment,
-        ]
+            # Добавить новую строку с данными отчета
+            worksheet.append_row(new_row, value_input_option="USER_ENTERED")
 
-        # Добавить новую строку с данными отчета
-        worksheet.append_row(new_row, value_input_option="USER_ENTERED")
-        logger.info(f"Отчет успешно отправлен: {new_row}")
-
-        return JSONResponse(content={"message": "Отчет успешно отправлен!"})
+            return JSONResponse(content={"message": "Отчет успешно отправлен!"})
 
     except Exception as e:
         logger.error(f"Ошибка при отправке отчета: {str(e)}")
         return JSONResponse(content={"message": "Ошибка сервера"}, status_code=500)
+
