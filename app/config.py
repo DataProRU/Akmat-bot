a80299a1ec0fb5dda4ffbf54ba7371ea95806c0e
Goryacheva Anastasia <nastgor12@gmail.com>
2025-02-21 22:00:54 +0200

    Changed tinkoff expeses endpoint and deleted .env

--- app/config.py
+++ app/config.py
@@ -1,15 +1,5 @@
 # /app/config.py
 
-# Стандартные модули Python
-import os
-
-# Сторонние модули
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-
 BASE_URL = "https://flight-trip-pavel-ershov-12.amvera.io/"
 WEB_APP_URL = "https://web-app-ax-pavel-ershov-12.amvera.io/"
 TOKEN = "7919291411:AAENTmsx7h0trc4ezowcqxX7LEyTn09WIWI"
@@ -18,5 +8,5 @@ TOKEN = "7919291411:AAENTmsx7h0trc4ezowcqxX7LEyTn09WIWI"
 ADD_CHAT_ID_URL = f"{BASE_URL}bot/check_access/"
 
 # Эндпоинты для тинькофф
-TINKOFF_EXPENSES_URL = f"{BASE_URL}bot/expenses/"
+TINKOFF_EXPENSES_URL = f"{BASE_URL}tinkoff/expenses/"
 TINKOFF_LOGIN_URL = f"{BASE_URL}tinkoff/"
